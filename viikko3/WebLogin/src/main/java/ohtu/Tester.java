c6e4080a0e9c8848277c24547f73dd5e316daf6e
Mona Kulmala <mlkulmala@hotmail.com>
2021-11-25 13:04:49 +0200

    koodi muutettu vastaamaan testejä

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -4,31 +4,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:4567");
+
+        System.out.println(driver.getPageSource());
         
-        sleep(2);
+        //sleep(2);
         
-        WebElement element = driver.findElement(By.linkText("login"));
+        WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
 
-        sleep(2);
+        System.out.println(driver.getPageSource());
+
+        //sleep(2);
 
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("pappa");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element.sendKeys("pappa1rainen");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("pappa1rainen");
+        element = driver.findElement(By.name("signup"));
         
         sleep(2);
         element.submit();
 
         sleep(3);
+        WebElement element2 = driver.findElement(By.linkText("continue to application mainpage"));
+        element2.click();
+
+        sleep(4);
+        WebElement element3 = driver.findElement(By.linkText("logout"));
+        element3.click();
         
         driver.quit();
     }
--- viikko3/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
+++ viikko3/WebLogin/src/main/java/ohtu/authentication/AuthenticationService.java
@@ -33,6 +33,22 @@ public class AuthenticationService {
         if (username.length()<3 ) {
             status.addError("username should have at least 3 characters");
         }
+        
+        if (!username.matches("^[a-z]+$")) {
+            status.addError("username should consist of letters a-z");
+        }
+        
+        if (password.length() < 8) {
+            status.addError("password should have at least 8 characters");
+        }
+        
+        if (password.matches("^[a-zA-Z]+$")) {
+            status.addError("username should consist of letters only");
+        }
+        
+        if (!password.equals(passwordConfirmation)) {
+            status.addError("password and password confirmation do not match");
+        }
 
         if (status.isOk()) {
             userDao.add(new User(username, password));
